ee748dbd8057ff1a2ad390951d1b88bc2a771a69
Dionisio Coelho <dionisiomeireles@gmail.com>
2024-03-11 09:19:18 +0000

    Putting the game in GitHub

---
+++ src/io/codeforall/javatars/Ácaro.java
@@ -0,0 +1,11 @@
+package io.codeforall.javatars;
+
+import io.codeforall.javatars.pokemons.Bug;
+
+public class Ácaro {
+    private Bug[] bugs = {new Bug(), new Bug()};
+
+    public Bug[] getBugs() {
+        return bugs;
+    }
+}
